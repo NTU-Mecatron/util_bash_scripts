f881b218a3235c49956aab7c656f1bd319625cf0
MecatronSW <mecatron.sw@gmail.com>
2025-06-17 20:05:24 +0800

    updated tmux settings

--- kevin_rise.sh
+++ kevin_rise.sh
@@ -20,12 +20,12 @@ if [ $? != 0 ]; then
   tmux split-window -h -t "$SESH":0.4   # pane 4 middle, pane 5 right
  
   # Commands for Window 0
-  tmux send-keys -t "$SESH":0.0 'roslaunch tf_manager tf-manager.launch'
-  #tmux send-keys -t "$SESH":0.1 'spare' C-m
-  #tmux send-keys -t "$SESH":0.2 'spare' C-m
-  tmux send-keys -t "$SESH":0.3 'roslaunch bt_planner bt_planner.launch'
-  #tmux send-keys -t "$SESH":0.4 'spare' C-m
-  tmux send-keys -t "$SESH":0.5 'roscore' C-m
+  tmux send-keys -t "$SESH":0.0 'roslaunch bt_planner pixhawk_bringup.launch'
+  tmux send-keys -t "$SESH":0.1 'roslaunch tf_manager tf_manager.launch'
+  tmux send-keys -t "$SESH":0.2 'roslaunch bt_planner starting.launch'
+  #tmux send-keys -t "$SESH":0.3 'spare'
+  #tmux send-keys -t "$SESH":0.4 'spare'
+  # tmux send-keys -t "$SESH":0.5 'spare'
 
   
   # Window 1
@@ -41,17 +41,17 @@ if [ $? != 0 ]; then
   tmux split-window -h -t "$SESH":1.4   # pane 4 middle, pane 5 right
 
   # Commands for Window 1
+  tmux send-keys -t "$SESH":1.0 'roscore' C-m
+  tmux send-keys -t "$SESH":1.1 './util_bash_scripts/foxglove_launch.sh'
+  tmux send-keys -t "$SESH":1.2 './util_bash_scripts/mavproxy.sh'
+  tmux send-keys -t "$SESH":1.3 'roslaunch bt_planner yolo_bringup.launch'
+  tmux send-keys -t "$SESH":1.4 'jtop' C-m
   #tmux send-keys -t "$SESH":1.0 'spare' C-m
-  #tmux send-keys -t "$SESH":1.1 'spare' C-m
-  #tmux send-keys -t "$SESH":1.2 'ping 192.168.0.'
-  tmux send-keys -t "$SESH":1.3 'jtop' C-m
-  tmux send-keys -t "$SESH":1.4 'roslaunch foxglove_bridge foxglove_bridge.launch' C-m
-  tmux send-keys -t "$SESH":1.5 './util_bash_scripts/mavproxy.sh' C-m
 
 
 fi
 
 # Attach to session
-tmux select-window -t "$SESH":0
-tmux select-pane -t "$SESH":0.0
+tmux select-window -t "$SESH":1
+tmux select-pane -t "$SESH":1.1
 tmux attach-session -t "$SESH"
